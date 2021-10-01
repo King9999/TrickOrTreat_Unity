6b92faf2d8b3d8b2783a122266569bac1d6e7a06
Mike Murray <mikemurray056@gmail.com>
2021-10-01 10:50:22 -0400

    Added Princess trick; adjusted house collision box

--- Assets/Scripts/Princess.cs
+++ Assets/Scripts/Princess.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /* The princess does not have a trick, but gets more candy from houses, and has higher move speed */
 public class Princess : Costume
 {
+    public GameObject questionMarkPrefab;
+    GameObject questionMark;
+    float trickPosOffset = 0.4f;
+
     // Start is called before the first frame update
     void Start()
     {
         name = "Princess";
         candyAmount = 0;
-        cooldown = 1;
+        initCooldown = 1;
         dropAmount = 5;
         candyTaken = 2;
         moveSpeed = 1.5f;
@@ -20,6 +25,37 @@ public class Princess : Costume
     // Update is called once per frame
     void Update()
     {
-        
+        //if trick is active, it will follow the player wherever they move.
+        if (isTrickActive)
+        {
+            questionMark.transform.position = new Vector3(transform.position.x, transform.position.y + trickPosOffset, PlayerZValue);
+        }
+
+        //check action timer
+        if (isTrickActive && Time.time > currentTime + actionTimer)
+        {
+            isTrickActive = false;
+            Destroy(questionMark);
+        }
+    }
+
+    /* The Princess doesn't have a trick, but something amusing should happen when player tries to use it */
+    public override void UseTrick(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            if (!isTrickActive && Time.time > currentTime + cooldown)
+            {
+                isTrickActive = true;
+                cooldown = initCooldown;
+                currentTime = Time.time;
+
+                
+                questionMark = Instantiate(questionMarkPrefab, new Vector3(transform.position.x, transform.position.y + trickPosOffset, PlayerZValue), Quaternion.identity);
+            }
+            //else
+            //play a sound or show a graphic to indicate trick can't be used yet.
+
+        }
     }
 }
