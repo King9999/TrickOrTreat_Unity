f42fef64a34cab225121c719e6b948417de63365
Mike Murray <mikemurray056@gmail.com>
2021-10-10 12:31:38 -0400

    Houses now have a 50% chance to be restocked
    This is to prevent houses from never getting stocked up even when they're ready to have candy.
    -Also changed the colliders on the house back to Dynamic since the Witch's projectiles were not being destroyed.

--- Assets/Scripts/HouseManager.cs
+++ Assets/Scripts/HouseManager.cs
@@ -7,12 +7,13 @@ using TMPro;
 public class HouseManager : MonoBehaviour
 {
     public House[] houses;
-    public TextMeshProUGUI[] candyUI;       //displays candy stock
+    public TextMeshProUGUI[] candyUI;               //displays candy stock
     public int housesWithCandy;                    //used to check when it's time for houses to stock up
 
     //consts
     public int TotalHousesWithCandy { get; } = 5;       //the maximum amount of houses that can have candy.
     public int TotalHouses { get; } = 10;
+    public float StockUpChance { get; } = 0.5f;
 
     public static HouseManager instance;
 
@@ -27,39 +28,6 @@ public class HouseManager : MonoBehaviour
         instance = this;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //houses = new House[TotalHouses];
-        //decide which houses have candy
-
-        //house setup
-        /*housesWithCandy = 0;
-        for (int i = 0; i < TotalHouses; i++)
-        {
-            //candy counter setup
-            //Vector3 worldPos = Camera.main.ScreenToWorldPoint(houses[i].transform.position);
-            //candyUI[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(worldPos.x, worldPos.y + 20);
-
-            //does this house have candy?
-            if (housesWithCandy < TotalHousesWithCandy)
-            {
-                float randNum = Random.Range(0f, 1f);
-                if (randNum <= 0.5f)
-                {
-                    houses[i].canHaveCandy = true;
-                    housesWithCandy++;
-                }
-                else
-                    houses[i].canHaveCandy = false;
-
-            }
-            else
-            {
-                houses[i].canHaveCandy = false;
-            }
-        }*/
-    }
 
     // Update is called once per frame
     void Update()
@@ -72,17 +40,17 @@ public class HouseManager : MonoBehaviour
 
             if (houses[i].canHaveCandy && !houses[i].HasCandy() && housesWithCandy < TotalHousesWithCandy)
             {
-                
-                houses[i].StockUp(); 
-                housesWithCandy++;              
+                //Roll to see if this house gets candy
+                float randNum = Random.Range(0f, 1f);
+                if (randNum <= StockUpChance)
+                {
+                    houses[i].StockUp();
+                    housesWithCandy++;
+                }
             }
-       // }
-            //housesWithCandy = 0;
-        //for (int i = 0; i < TotalHouses; i++)
-        //{
+      
             if (houses[i].HasCandy())
             {
-                //housesWithCandy++;
                 if (houses[i].candyAmountIsHidden)
                 {
                     candyUI[i].text = "??";
@@ -98,19 +66,6 @@ public class HouseManager : MonoBehaviour
             }
 
         }
-
-        //check if any new houses can have candy and turn on lights.
-        /*while (housesWithCandy < TotalHousesWithCandy)
-        {
-            for (int i = 0; i < TotalHouses; i++)
-            {
-                if (houses[i].canHaveCandy)
-                {
-                    StockUp();
-                }
-            }
-
-        }*/
 
     }
 }
