3f668b4cba4c13539464e9a118c4b6fd0c64b307
Mike Murray <mikemurray056@gmail.com>
2021-10-21 16:44:49 -0400

    Cursor control added to select screen
    Would like to figure out how to prevent cursors from overlapping each other.

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -1,17 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
-//Player selects their costume here. A description of the costume and their trick can be found here also.
-public class CostumeSelect : MonoBehaviour
+public class Cursor : MonoBehaviour
 {
-    //public TextMeshProUGUI costumeDescription;
-    //public TextMeshProUGUI trickData;
-    string[] costumeDescriptions = new string[4];
-    string[] trickDescriptions = new string[4];
+    public Image[] costumes = new Image[MAX_COSTUMES];  //contain references to the costumes on select screen. Needed for cursor placement.
 
     enum CostumeType
     {
@@ -20,19 +15,30 @@ public class CostumeSelect : MonoBehaviour
 
     CostumeType currentCostume;
 
+    //consts
+    const int MAX_COSTUMES = 4;
+    //const int GHOST = 0;
+    //const int KNIGHT = 1;           //index locations in array
+    //const int PRINCESS = 2;
+    //const int WITCH = 3;
+    const float yOffset = 75;
+    const float xOffset = -10;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        //set initial cursor position. They can occupy same space but players must select different costumes.
+        transform.position = new Vector3(costumes[(int)CostumeType.Ghost].transform.position.x + xOffset, costumes[(int)CostumeType.Ghost].transform.position.y + yOffset, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //update cursor's location if necessary
+        transform.position = new Vector3(costumes[(int)currentCostume].transform.position.x + xOffset, costumes[(int)currentCostume].transform.position.y + yOffset, transform.position.z);
     }
 
-    /*public void OnPressLeft(InputAction.CallbackContext context)
+    public void OnPressLeft(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
@@ -43,12 +49,12 @@ public class CostumeSelect : MonoBehaviour
 
             //move cursor to next costume
             //cursor.transform.position = menus[currentMenu];
-            
+
             Debug.Log("Pressed Left");
         }
-    }*/
+    }
 
-    /*public void OnPressRight(InputAction.CallbackContext context)
+    public void OnPressRight(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
@@ -62,14 +68,14 @@ public class CostumeSelect : MonoBehaviour
 
             Debug.Log("Pressed Right");
         }
-    }*/
+    }
 
-    /*public void OnSelectedCostume(InputAction.CallbackContext context)
+    public void OnSelectedCostume(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
             //check what player 1 and 2 selected and carry that info over to game screen.
-            if (currentMenu == START)
+            /*if (currentMenu == START)
             {
                 //load game
                 StartCoroutine(OpenScene("Game"));
@@ -87,8 +93,9 @@ public class CostumeSelect : MonoBehaviour
                 if (muted == false)
                     //play sound to alert player that sound is on.
                     soundSource.Play();
-            }
+            }*/
             //Debug.Log("Selected Menu Option");
         }
-    }*/
+    }
+
 }
